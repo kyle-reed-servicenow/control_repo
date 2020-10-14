3adb6ca6525a99be68d5a6d92c3a9caaac4b7ba6
kyle-reed-servicenow <72846727+kyle-reed-servicenow@users.noreply.github.com>
2020-10-13 22:21:59 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,11 +1,18 @@
 node default {
+  file { '/root/README' :
+    ensure  => file,
+    content => 'This is a readme',
+    owner   => 'root',
+  }
 }
 node 'master.puppet.vm' {
   include role::master_server
 }
+
 node 'web.puppet.vm' {
   include role::app_server
 }
+
 node 'db.puppet.vm' {
   include role::db_server
 }
